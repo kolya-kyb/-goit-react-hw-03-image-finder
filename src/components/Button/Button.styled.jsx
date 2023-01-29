1c5fbc71d4914359a2c605c786adde4a5b7d3691
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2023-01-29 18:21:30 +0200

    update

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const ButtonStyled = styled.button`
   padding: 8px 16px;
+
   border-radius: 2px;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
